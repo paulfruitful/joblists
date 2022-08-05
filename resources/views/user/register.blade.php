dd549f65c55a540b86049f8325a3e5b7a6d628f1
Paul Fruitful <fruitful2007@outlook.com>
2022-08-06 00:30:35 +0100

    Updated the registration page

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -35,34 +35,34 @@
     </section>
     <form class="w-full max-w-sm mx-auto m-48 p-6 border rounded  border-indigo-600" id="register">
         <div class="p-6"><p class="text-center text-red-500  text-2xl font-bold">Register</p></div>
-        <div class="flex flex-wrap -mx-6 mb-6">
-            <div class="w-full md:w-1/2 px-3">
-                <label class="block uppercase tracking-wide text-indigo-700 text-xs font-bold mb-2" for="grid-last-name">
-                  First Name
-                </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="John" name="firstname">
-              </div>
-          <div class="w-full md:w-1/2 px-3">
-            <label class="block uppercase tracking-wide text-indigo-700 text-xs font-bold mb-2" for="grid-last-name">
-              Last Name
-            </label>
-            <input class="appearance-none block w-full bg-indigo-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
-          </div>
-        </div>
+        
         <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+                <label class="block uppercase tracking-wide text-indigo-700 text-xs font-bold mb-2" for="grid-password">
+                  Full Name
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="John Doe" name="name">
+                @error('name')
+                <div class="w-full"><p class="text-red-500">{{$message}}</p></div>
+                   @enderror
+            </div>
             <div class="w-full px-3">
                 <label class="block uppercase tracking-wide text-indigo-700 text-xs font-bold mb-2" for="grid-password">
                   Email
                 </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="email" placeholder="youreamil@example.com">
-               
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="email" placeholder="youreamil@example.com" name="email">
+               @error('email')
+            <div class="w-full"><p class="text-red-500">{{$message}}</p></div>
+               @enderror
               </div>
           <div class="w-full px-3">
             <label class="block uppercase tracking-wide text-indigo-700 text-xs font-bold mb-2" for="grid-password">
               Password
             </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************">
-           
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" name="password">
+            @error('password')
+            <div class="w-full"><p class="text-red-500">{{$message}}</p></div>
+               @enderror
           </div>
 
         </div>
