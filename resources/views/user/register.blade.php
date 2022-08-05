347385ef9bca13a2e118a15fd203b1f885f66295
Paul Fruitful <fruitful2007@outlook.com>
2022-08-05 23:59:39 +0100

    Addition of a new column in the users table and the update of the registration page

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -40,7 +40,7 @@
                 <label class="block uppercase tracking-wide text-indigo-700 text-xs font-bold mb-2" for="grid-last-name">
                   First Name
                 </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="John">
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="John" name="firstname">
               </div>
           <div class="w-full md:w-1/2 px-3">
             <label class="block uppercase tracking-wide text-indigo-700 text-xs font-bold mb-2" for="grid-last-name">
