645e3023de0def74f0887d8494256f1cd51f7fd1
Paul Fruitful <fruitful2007@outlook.com>
2022-08-08 04:24:53 +0100

    Job listings for users

--- app/Http/Controllers/listControl.php
+++ app/Http/Controllers/listControl.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Listing;
 
 class listControl extends Controller
 {
@@ -13,7 +14,8 @@ class listControl extends Controller
      */
     public function index()
     {
-        //
+        $list=Listing::latest()->paginate()->get();
+        return view('pages.listing',['list'=>$list]);
     }
 
     /**
