80cf8c1f3e21df3e5fec2310a0710d55bc4501bf
Paul Fruitful <fruitful2007@outlook.com>
2022-08-08 03:44:55 +0100

    User Dashboard UI

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,3 +1,3 @@
 <div class="text-center bg-indigo-900 text-white py-24 px-6">
-    <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">Welcome Back<br /><span class="text-red-400">{{}}</span></h1>
+    <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">Welcome Back<br /><span class="text-red-400">{{$user->name}}</span></h1>
  </div>
