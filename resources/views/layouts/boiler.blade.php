7dcb2e4aa02bb171417e0ae6d927b66ba65b3238
Paul Fruitful <fruitful2007@outlook.com>
2022-08-08 03:26:47 +0100

    User Dashboard UI

--- resources/views/layouts/boiler.blade.php
+++ resources/views/layouts/boiler.blade.php
@@ -32,9 +32,9 @@ x-data="{open:false,menu:false, lokasi:false}">
     <div class="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
       <div class="relative flex items-center justify-between h-16">
         <div class="flex items-center px-2 lg:px-0">
-          <a class="flex-shrink-0" href="#">
-            <img class="block lg:hidden h-12 w-16" src="https://imlovefood.com/wp-content/themes/mypanganthema/img/logo_small.png" alt="Logo">
-            <img class="hidden lg:block h-12 w-auto" src="https://imlovefood.com/wp-content/themes/mypanganthema/img/logo_small_gray.png" alt="Logo">
+          <a class="flex-shrink-0" href="/">
+            <img class="block lg:hidden h-12 w-16" src="https://www.kindpng.com/picc/m/133-1337806_job-vector-seeker-job-logo-magnifying-glass-png.png" alt="Logo">
+            <img class="hidden lg:block h-12 w-auto" src="https://www.kindpng.com/picc/m/133-1337806_job-vector-seeker-job-logo-magnifying-glass-png.png" alt="Logo">
           </a>
           <div class="hidden lg:block lg:ml-2">
             <div class="flex">
