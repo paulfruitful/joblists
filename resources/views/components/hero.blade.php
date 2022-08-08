378d8ae17283fc2a4a75f8c8117b293f641e230c
Paul Fruitful <fruitful2007@outlook.com>
2022-08-08 04:09:41 +0100

    User Dashboard UI

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,15 +1,28 @@
-<div class="grid lg:grid-cols-2 gap-12 flex items-center">
-    <div class="mt-12 lg:mt-0">
-      <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">The best offer <br /><span class="text-blue-600">for your business</span></h1>
-      <a class="inline-block px-7 py-3 mr-2 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">Get started</a>
-      <a class="inline-block px-7 py-3 bg-transparent text-blue-600 font-medium text-sm leading-snug uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">Learn more</a>
-    </div>
-    <div class="mb-12 lg:mb-0">
-      <img
-        src="https://www.lifewire.com/thmb/kXpBXHj4IwK5bA9ii_xVMHLvkZs=/1500x1000/filters:fill(auto,1)/Working-on-a-laptop-56770e2c3df78ccc15278af7.jpg"
-        class="w-full rounded-lg shadow-lg"
-        alt=""
-      />
+<div
+class="relative overflow-hidden bg-no-repeat bg-cover"
+style="
+  background-position: 50%;
+  background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/146.webp');
+  height: 500px;
+"
+>
+<div
+  class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
+  style="background-color: rgba(0, 0, 0, 0.75)"
+>
+  <div class="flex justify-center items-center h-full">
+    <div class="text-center text-white px-6 md:px-12">
+      <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">
+        The best offer on the market <br /><span>for your business</span>
+      </h1>
+      <button
+        type="button"
+        class="inline-block px-7 py-3 border-2 border-white text-white font-medium text-sm leading-snug uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+        data-mdb-ripple="true"
+        data-mdb-ripple-color="light"
+      >
+        Get started
+      </button>
     </div>
   </div>
 </div>
