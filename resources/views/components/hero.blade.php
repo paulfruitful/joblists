88ec45fd8905c5617452de98122f19eebd063eb9
Paul Fruitful <fruitful2007@outlook.com>
2022-08-08 04:19:16 +0100

    User Dashboard UI

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -13,16 +13,16 @@ style="
   <div class="flex justify-center items-center h-full">
     <div class="text-center text-white px-6 md:px-12">
       <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">
-        The best offer on the market <br /><span>for your business</span>
+        Welcome To Joblists<br /><span>Get Ready To Land A Job!</span>
       </h1>
-      <button
-        type="button"
-        class="inline-block px-7 py-3 border-2 border-white text-white font-medium text-sm leading-snug uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+      <a
+      href="#"
+        class="inline-block px-7 py-3 border-2 border-white text-white font-medium  text-sm leading-snug uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
         data-mdb-ripple="true"
         data-mdb-ripple-color="light"
       >
-        Get started
-      </button>
+        Got A Job To List
+    </a>
     </div>
   </div>
 </div>
