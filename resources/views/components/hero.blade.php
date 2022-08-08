0a9371bc4fcd7571cc06ed803a8df1d089bf1ca4
Paul Fruitful <fruitful2007@outlook.com>
2022-08-08 04:07:16 +0100

    User Dashboard UI

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,3 +1,16 @@
-<div class="text-center bg-indigo-900 text-white py-24 px-6">
-    <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">Welcome Back<br /><span class="text-red-400">User</span></h1>
- </div>
+<div class="grid lg:grid-cols-2 gap-12 flex items-center">
+    <div class="mt-12 lg:mt-0">
+      <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">The best offer <br /><span class="text-blue-600">for your business</span></h1>
+      <a class="inline-block px-7 py-3 mr-2 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">Get started</a>
+      <a class="inline-block px-7 py-3 bg-transparent text-blue-600 font-medium text-sm leading-snug uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">Learn more</a>
+    </div>
+    <div class="mb-12 lg:mb-0">
+      <img
+        src="https://mdbootstrap.com/img/new/ecommerce/vertical/028.jpg"
+        class="w-full rounded-lg shadow-lg"
+        alt=""
+      />
+    </div>
+  </div>
+</div>
+</div>
