0e91e23ce641d0cbf16cef96f48ff7fb588ca18a
Paul Fruitful <fruitful2007@outlook.com>
2022-08-15 23:45:13 +0100

    Rebuilding show single listing page

--- resources/views/components/job-list.blade.php
+++ resources/views/components/job-list.blade.php
@@ -21,19 +21,9 @@
                   </svg>Hot news
               </p>
               <p class="text-gray-500 mb-6">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum maxime voluptas
-                ipsam aliquam itaque cupiditate provident architecto expedita harum culpa odit,
-                inventore rem molestias laborum repudiandae corporis pariatur quo eius iste!
-                Quaerat, assumenda voluptates! Molestias, recusandae? Maxime fuga omnis ducimus.
+                {{$list->description}}
               </p>
-              <p class="text-gray-500">
-                Commodi ut nisi assumenda alias maxime necessitatibus ad rem repellat explicabo,
-                reiciendis illum suscipit iusto? Provident dignissimos similique, reiciendis
-                inventore accusantium unde mollitia, deleniti quae atque error id perspiciatis
-                illum. Laboriosam aperiam ab illo dignissimos obcaecati corporis similique a odio,
-                optio iste quis placeat alias amet rerum sint quos dolor pariatur inventore
-                possimus ad consequuntur fugiat perferendis consectetur laudantium.
-              </p>
+              
             </div>
           </div>
         </div>
