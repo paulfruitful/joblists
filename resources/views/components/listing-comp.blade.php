e7d24c1add31aad984739648e581f694947e4c21
Paul Fruitful <fruitful2007@outlook.com>
2022-08-17 12:34:47 +0100

    Edited the show listings page

--- resources/views/components/listing-comp.blade.php
+++ resources/views/components/listing-comp.blade.php
@@ -10,9 +10,9 @@
 <div class="max-w-lg ">
 <div class="border-2 border-gray box-content block border-rounded  p-12 mb-6 flex hover:border-white w-screen shadow-lg shadow-blacks w-full ">
    <a href="/{{$list->id}}"> <h2 class="text-2xl text-black  " >{{$list->title}}</h2></a>
-   
-   <a href="/{{$list->id}}" class="bg-indigo-800 p-3 rounded-md ml-12 inline-block  text-white relative bottom-0 left-32 ">Apply</a>
-
+   <div class="relative">
+   <a href="/{{$list->id}}" class="bg-indigo-800 p-3 rounded-md ml-12 inline-block  text-white relative bottom-0 -left-9 ">Apply</a>
+   </div>
 </div>
 </div>
 @endforeach
