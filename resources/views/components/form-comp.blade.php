6d755ae32dbe7c6d983db5ae7cfc3714b7316e6e
Paul Fruitful <fruitful2007@outlook.com>
2022-08-23 02:39:19 +0100

    Adding the creation of Job listings feature

--- resources/views/components/form-comp.blade.php
+++ resources/views/components/form-comp.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="md:grid md:grid-cols-3 md:gap-6">
+    <div class="md:grid md:grid-cols-3 md:gap-6 m-6" >
       <div class="md:col-span-1">
         <div class="px-4 sm:px-0">
           <h3 class="text-lg font-medium leading-6 text-gray-900">Profile</h3>
