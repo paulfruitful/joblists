ee85137b6a85bd05d05a07fee040b36ad6fd9a6d
Paul Fruitful <fruitful2007@outlook.com>
2022-08-23 02:49:58 +0100

    Adding the creation of Job listings feature

--- resources/views/components/form-comp.blade.php
+++ resources/views/components/form-comp.blade.php
@@ -25,7 +25,7 @@
               </div>
   
               <div>
-                <label for="about" class="block text-sm font-medium text-gray-700"> About </label>
+                <label for="about" class="block text-sm font-medium text-gray-700"> Job Description </label>
                 <div class="mt-1">
                   <textarea id="about" name="about" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="you@example.com"></textarea>
                 </div>
