09e1394136f0000a7f2c2d4bc26c346a65c31dd1
Paul Fruitful <fruitful2007@outlook.com>
2022-08-23 02:20:21 +0100

    Adding the creation of Job listings feature

--- resources/views/components/form-comp.blade.php
+++ resources/views/components/form-comp.blade.php
@@ -1,3 +1,5 @@
 <div class="p-6 items-center max-w-sm flex-row">
-<form action="/create" method="post"></form>
+<form action="/create" method="post" class="flex-row">
+<input type="text" name="title" class="">
+</form>
 </div>
