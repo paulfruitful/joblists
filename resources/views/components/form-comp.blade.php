2b031deaa5e227f9cc3728e6ad04f19efc9b40ff
Paul Fruitful <fruitful2007@outlook.com>
2022-08-23 02:32:35 +0100

    Adding the creation of Job listings feature

--- resources/views/components/form-comp.blade.php
+++ resources/views/components/form-comp.blade.php
@@ -8,6 +8,7 @@
       </div>
       <div class="mt-5 md:mt-0 md:col-span-2">
         <form action="/create" method="POST">
+        @csrf
           <div class="shadow sm:rounded-md sm:overflow-hidden">
             <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
               <div class="grid grid-cols-3 gap-6">
