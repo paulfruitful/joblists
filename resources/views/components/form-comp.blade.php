e6923272f460934dcb04210161b6d8909fae0215
Paul Fruitful <fruitful2007@outlook.com>
2022-08-25 00:29:56 +0100

    Adding the creation of Job listings feature

--- resources/views/components/form-comp.blade.php
+++ resources/views/components/form-comp.blade.php
@@ -24,7 +24,7 @@
                 </div>
               </div>
               @error('company')
-                  <p class="text-red-400 text-sm">{{$message}}</p>
+                  <p class="text-red-500 text-sm">{{$message}}</p>
               @enderror
 
               <div class="grid grid-cols-3 gap-6">
