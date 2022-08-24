472a9301b0c9a0fe2fb3b381dbd9e2793044fd2e
Paul Fruitful <fruitful2007@outlook.com>
2022-08-24 23:37:36 +0100

    Adding the creation of Job listings feature

--- resources/views/components/form-comp.blade.php
+++ resources/views/components/form-comp.blade.php
@@ -14,8 +14,16 @@
         @csrf
           <div class="shadow sm:rounded-md sm:overflow-hidden">
             <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+              <div class="grid grid-cols-3 gap-6">
+                <div class="col-span-3 sm:col-span-2">
+                  <label for="company-name" class="block text-sm font-medium text-gray-700"> Company Name </label>
+                  <div class="mt-1 flex rounded-md shadow-sm">
+                    <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm"> http:// </span>
+                    <input type="text" name="website" id="company-website" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="www.example.com">
+                  </div>
+                </div>
+              </div>
 
-              
               <div class="grid grid-cols-3 gap-6">
 
                 <div class="col-span-3 sm:col-span-2">
