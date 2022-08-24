e3ce6d189b2e2dd7566fa101d992cfec73209767
Paul Fruitful <fruitful2007@outlook.com>
2022-08-24 23:39:41 +0100

    Adding the creation of Job listings feature

--- resources/views/components/form-comp.blade.php
+++ resources/views/components/form-comp.blade.php
@@ -49,7 +49,7 @@
                 <div class="mt-1">
                   <textarea id="about" name="about" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="you@example.com"></textarea>
                 </div>
-                <p class="mt-2 text-sm text-gray-500">Brief description for your profile. URLs are hyperlinked.</p>
+                <p class="mt-2 text-sm text-gray-500">Brief description for your profile</p>
               </div>
   
            
