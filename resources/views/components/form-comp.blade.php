4c2323d6523532829e43104fc261c0c1e6bf8024
Paul Fruitful <fruitful2007@outlook.com>
2022-08-24 23:50:53 +0100

    Adding the creation of Job listings feature

--- resources/views/components/form-comp.blade.php
+++ resources/views/components/form-comp.blade.php
@@ -52,7 +52,15 @@
                 <p class="mt-2 text-sm text-gray-500">Brief description for your profile</p>
               </div>
   
-           
+              <div class="grid grid-cols-3 gap-6">
+                <div class="col-span-3 sm:col-span-2">
+                  <label for="company-name" class="block text-sm font-medium text-gray-700">Location</label>
+                  <div class="mt-1 flex rounded-md shadow-sm">
+                  
+                    <input type="text" name="company"  class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="Microsoft">
+                  </div>
+                </div>
+              </div>
   
               <div>
                 <label class="block text-sm font-medium text-gray-700"> Cover photo </label>
