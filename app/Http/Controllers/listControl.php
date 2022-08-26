a8b378b21bbfc47253f25cdb26e14d03b921698e
Paul Fruitful <fruitful2007@outlook.com>
2022-08-26 02:42:45 +0100

    Adding the edit listing feature

--- app/Http/Controllers/listControl.php
+++ app/Http/Controllers/listControl.php
@@ -74,7 +74,7 @@ class listControl extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Listing $listing)
     {
         //
     }
