28b9e8a1b73412b2b9b902898b3883154ac9d0e6
Paul Fruitful <fruitful2007@outlook.com>
2022-08-29 22:55:56 +0100

    Adding the delete listing feature

--- app/Http/Controllers/listControl.php
+++ app/Http/Controllers/listControl.php
@@ -112,8 +112,8 @@ class listControl extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Listing $listing)
     {
-        //
+        
     }
 }
