0a6cc5e824012ac6a77ee85ae5dc0b4bcbcdacf7
Paul Fruitful <fruitful2007@outlook.com>
2022-08-29 23:40:30 +0100

    Completing the CRUD

--- resources/views/components/job-list.blade.php
+++ resources/views/components/job-list.blade.php
@@ -27,7 +27,8 @@
                   
               <a href="/{{$list->id}}/edit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">Edit</button>
             <form action="/{{$list->id}}/delete" method="post">
-              
+              @csrf
+              @method('DELETE')
               <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700">Delete</button>
             
             </form>
