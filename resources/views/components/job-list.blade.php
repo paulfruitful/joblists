c0f677f061bf14bed574e7544e5e0268d2f8ac32
Paul Fruitful <fruitful2007@outlook.com>
2022-08-29 23:30:42 +0100

    Completing the CRUD

--- resources/views/components/job-list.blade.php
+++ resources/views/components/job-list.blade.php
@@ -29,7 +29,7 @@
               <a href="/{{$listing->id}}/edit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Edit</button>
             <form action="/{{$listing->id}}/delete" method="post">
               
-              <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
+              <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
             
             </form>
               </div>
