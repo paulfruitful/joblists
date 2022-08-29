9d194884e4c65ee0e7e2e86c1f19a40b7846fcd1
Paul Fruitful <fruitful2007@outlook.com>
2022-08-29 23:48:52 +0100

    Completing the CRUD

--- resources/views/components/job-list.blade.php
+++ resources/views/components/job-list.blade.php
@@ -33,7 +33,9 @@
       
     </section>
     <!-- Section: Design Block -->
-     @if (auth()->id()==$list->user_id)
+   
+  </div> 
+   @if (auth()->id()==$list->user_id)
                   
               <a href="/{{$list->id}}/edit" class="inline-flex justify-center  mr-8 py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">Edit</button>
             <form action="/{{$list->id}}/delete" method="post">
@@ -45,4 +47,3 @@
             @else
                  <div></div> 
               @endif
-  </div>
