c92feaefc1d6df36a81ff7733fe81c75aa512ce5
Paul Fruitful <fruitful2007@outlook.com>
2022-08-30 00:00:17 +0100

    Completing the CRUD

--- app/Http/Controllers/listControl.php
+++ app/Http/Controllers/listControl.php
@@ -113,7 +113,7 @@ class listControl extends Controller
      */
     public function destroy(Listing $listing)
     {
-        $listing->destroy($listig->id);
+        $listing->destroy($listing->id);
         return redirect('/listing')->with('Success','You have successfully deleted a listing');
     }
 }
