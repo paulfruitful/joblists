182fef70793a6e18cf1762e7a18827cd13640914
Paul Fruitful <fruitful2007@outlook.com>
2022-08-31 01:26:58 +0100

    Creating the applications database

--- database/migrations/2022_08_31_002528_create_applications_table.php
+++ database/migrations/2022_08_31_002528_create_applications_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('applications', function (Blueprint $table) {
             $table->id();
+            $table->longText('');
             $table->timestamps();
         });
     }
