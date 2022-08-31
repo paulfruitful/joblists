311b709afeeabebfbb3ec08a9cb997b65a6f921a
Paul Fruitful <fruitful2007@outlook.com>
2022-08-31 19:39:20 +0100

    Creating the application Controller

--- app/Http/Controllers/applicationControl.php
+++ app/Http/Controllers/applicationControl.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class applicationControl extends Controller
 {
     public function create(Request $request){
+        
         return view('application')->with('listing',$listing);
     }
 }
