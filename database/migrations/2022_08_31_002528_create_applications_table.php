70f941af2f8bc02e8ad9b27c3842e13b8741743b
Paul Fruitful <fruitful2007@outlook.com>
2022-08-31 01:28:39 +0100

    Creating the applications database

--- database/migrations/2022_08_31_002528_create_applications_table.php
+++ database/migrations/2022_08_31_002528_create_applications_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->longText('coverletter');
+            $table->string('cv');
+
             $table->timestamps();
         });
     }
