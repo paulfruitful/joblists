2d3acca91b9018106b64b34a8b2ab1af69385f77
Paul Fruitful <fruitful2007@outlook.com>
2022-09-02 14:25:02 +0100

    Added the portfolio column to database

--- database/migrations/2022_08_31_002528_create_applications_table.php
+++ database/migrations/2022_08_31_002528_create_applications_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->longText('coverletter');
             $table->foreignId('listing_id')->constrained()->onDelete('cascade');
             $table->string('cv');
-            $tale->string('portfolio');
+            $table->string('portfolio');
 
             $table->timestamps();
         });
