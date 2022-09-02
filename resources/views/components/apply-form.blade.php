5958b26e43ce0a32e83ed95824a5c3ef2c16d1dd
Paul Fruitful <fruitful2007@outlook.com>
2022-09-02 14:48:13 +0100

    Working the application UI

--- resources/views/components/apply-form.blade.php
+++ resources/views/components/apply-form.blade.php
@@ -53,7 +53,7 @@
               <div>
                 <label for="about" class="block text-sm font-medium text-gray-700"> Cover Letter</label>
                 <div class="mt-1">
-                  <textarea  name="description" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="  We need an experiend..."></textarea>
+                  <textarea  name="coverletter" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="  We need an experiend..."></textarea>
                 </div>
                 <p class="mt-2 text-sm text-gray-500">Brief description for your profile</p>
               </div>
