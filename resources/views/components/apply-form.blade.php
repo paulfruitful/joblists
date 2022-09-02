9308f205ed0e653926f8dd10c5909393984dfd7e
Paul Fruitful <fruitful2007@outlook.com>
2022-09-02 14:22:22 +0100

    The application form UI

--- resources/views/components/apply-form.blade.php
+++ resources/views/components/apply-form.blade.php
@@ -109,7 +109,7 @@
                       </label>
                       <p class="pl-1">or drag and drop</p>
                     </div>
-                    <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+                    <p class="text-xs text-gray-500">PDf, DOCX, or XML format</p>
                   </div>
                 </div>
               </div>
