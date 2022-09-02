0a80b2d26ea5a1408cd6e451edd03f57f1b426ab
Paul Fruitful <fruitful2007@outlook.com>
2022-09-02 16:56:06 +0100

    Working the application UI

--- app/Http/Controllers/listControl.php
+++ app/Http/Controllers/listControl.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class listControl extends Controller
 {
