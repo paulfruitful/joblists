8ea165a48966e0152bbf3697715fcf7a43ee5926
Paul Fruitful <fruitful2007@outlook.com>
2022-09-02 14:47:52 +0100

    Working the application UI

--- resources/views/components/apply-form.blade.php
+++ resources/views/components/apply-form.blade.php
@@ -51,7 +51,7 @@
           @enderror
               
               <div>
-                <label for="about" class="block text-sm font-medium text-gray-700"> Job Description </label>
+                <label for="about" class="block text-sm font-medium text-gray-700"> Cover Letter</label>
                 <div class="mt-1">
                   <textarea  name="description" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="  We need an experiend..."></textarea>
                 </div>
