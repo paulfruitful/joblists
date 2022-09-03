b95d3eb1e418a39cdd8a56873dd07d5e10f8a6f4
Paul Fruitful <fruitful2007@outlook.com>
2022-09-03 23:09:25 +0100

    The application form UI

--- resources/views/components/apply-form.blade.php
+++ resources/views/components/apply-form.blade.php
@@ -62,15 +62,7 @@
               @error('coverletter')
               <p class="text-red-500 text-sm">{{$message}}</p>
           @enderror
-              <div class="grid grid-cols-3 gap-6">
-                <div class="col-span-3 sm:col-span-2">
-                  <label for="company-name" class="block text-sm font-medium text-gray-700">Location</label>
-                  <div class="mt-1 flex rounded-md shadow-sm">
-                  
-                    <input type="text" name="location"  class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="23 Orsborne Rd, Lagos">
-                  </div>
-                </div>
-              </div>
+              
               <div class="grid grid-cols-3 gap-6">
 
                 <div class="col-span-3 sm:col-span-2">
