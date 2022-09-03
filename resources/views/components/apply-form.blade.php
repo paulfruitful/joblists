f34f5563bdcff902b48bca19f93deaee3fa0e6f3
Paul Fruitful <fruitful2007@outlook.com>
2022-09-03 21:41:07 +0100

    The application form UI

--- resources/views/components/apply-form.blade.php
+++ resources/views/components/apply-form.blade.php
@@ -23,7 +23,7 @@
                   </div>
                 </div>
               </div>
-              @error('company')
+              @error('name')
                   <p class="text-red-500 text-sm">{{$message}}</p>
               @enderror
 
