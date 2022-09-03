7152b7480c8933c8a14a46dd2584737c0421158c
Paul Fruitful <fruitful2007@outlook.com>
2022-09-03 23:12:12 +0100

    The application form UI

--- resources/views/components/apply-form.blade.php
+++ resources/views/components/apply-form.blade.php
@@ -66,10 +66,7 @@
               <div class="grid grid-cols-3 gap-6">
 
                 <div class="col-span-3 sm:col-span-2">
-                  <label for="company-website" class="block text-sm font-medium text-gray-700">Tags</label>
-                  <div class="mt-1 flex rounded-md shadow-sm">
-                   <input type="text" name="tags"  class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="Node.js, Laravel, HTML">
-                  </div>
+                  
                 </div>
               </div>
               <div>
