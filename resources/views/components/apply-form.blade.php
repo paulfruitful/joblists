2e568450becdc8e7e0a90c1fdd4fe6baf8bd6025
Paul Fruitful <fruitful2007@outlook.com>
2022-09-03 23:14:03 +0100

    The application form UI

--- resources/views/components/apply-form.blade.php
+++ resources/views/components/apply-form.blade.php
@@ -70,7 +70,7 @@
                 </div>
               </div>
               
-                <label class="block text-sm font-medium text-gray-700"> Cover photo </label>
+                <label class="block text-sm font-medium text-gray-700"> CV</label>
                 <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
                   <div class="space-y-1 text-center">
                     <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
