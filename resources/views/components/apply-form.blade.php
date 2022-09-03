7b1fac943aaba48945c76fa3a80f59a4e7a36327
Paul Fruitful <fruitful2007@outlook.com>
2022-09-03 22:16:18 +0100

    The application form UI

--- resources/views/components/apply-form.blade.php
+++ resources/views/components/apply-form.blade.php
@@ -41,7 +41,7 @@
           @enderror
               <div class="grid grid-cols-3 gap-6">
                 <div class="col-span-3 sm:col-span-2">
-                  <label for="company-website" class="block text-sm font-medium text-gray-700"> Website </label>
+                  <label for="company-website" class="block text-sm font-medium text-gray-700"> Portfolio </label>
                   <div class="mt-1 flex rounded-md shadow-sm">
                     <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm"> http:// </span>
                     <input type="text" name="website" id="company-website" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="www.example.com">
