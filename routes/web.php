ab4172ece4e2b41ed01749400e82cb4217ea7014
Paul Fruitful <fruitful2007@outlook.com>
2022-09-05 00:31:04 +0100

    Creating the route and view to the all jobs page

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::post('/login',[userController::class,'login'])->name('login');
 
 Route::get('/listings',function(){
     return view('joblists',[
-        'list'=>Listing::all()
+        'list'=>Listing::latest()
     ]);
 });
 //The route to get the listing creation form 
