367a268f77416a0c70b1e20f8341c34cc4470c05
Paul Fruitful <fruitful2007@outlook.com>
2022-09-05 00:11:26 +0100

    Adding the flash messages

--- resources/views/components/popup-message.blade.php
+++ resources/views/components/popup-message.blade.php
@@ -1,6 +1,6 @@
 @if(session()->has('Success'))
 <div x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show"
-  class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-green-500 text-black text-md font-semibold px-48 py-3">
+  class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-green-500 text-white text-md font-semibold px-48 py-3">
   <p>
     {{session('Success')}}
   </p>
