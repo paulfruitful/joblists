36f8de5c18a341514ad21d2cd481e885720c5883
Paul Fruitful <fruitful2007@outlook.com>
2022-09-05 00:30:00 +0100

    Creating the route and view to the all jobs page

--- resources/views/joblists.blade.php
+++ resources/views/joblists.blade.php
@@ -24,6 +24,5 @@
     </head>
     <body>
         <x-nav-nav/>
-        <p class="m-4 p-6 text-center font-bold text-2xl"> Get The Most Current Jobs</p>
     <x-listing-comp :listing="$list"/>
     </body>
