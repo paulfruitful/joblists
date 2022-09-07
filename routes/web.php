ba806f0138614cabbc30c94db6755f0a3a3b57bf
Paul Fruitful <fruitful2007@outlook.com>
2022-09-07 01:51:54 +0100

    adding the route for users to view all their applications

--- routes/web.php
+++ routes/web.php
@@ -43,9 +43,7 @@ Route::get('/manage',function(){
         'listings'=>auth()->user()->listing
     ]);
 });
-Route::get('/applications', function(){
-    
-});
+Route::get('/applications',[]);
 //The route to get the listing creation form 
 Route::get('/create',[listControl::class,'create']);
 //The route to create a job listing
