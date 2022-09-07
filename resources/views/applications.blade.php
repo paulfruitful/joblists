f68e8f6c45469e7f146fe4b1d92c983bad850a74
Paul Fruitful <fruitful2007@outlook.com>
2022-09-07 14:09:55 +0100

    the aplication view creation

--- resources/views/applications.blade.php
+++ resources/views/applications.blade.php
@@ -26,12 +26,32 @@
         <x-nav-nav/>
         <div class="max-w-lg"></div>
      @if (count($applications)>0)
-         <table>
-            <tr>
-
-            </tr>
-
-         </table>
+     <table class="table-auto">
+        <thead>
+          <tr>
+            <th>Song</th>
+            <th>Artist</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+            <td>Malcolm Lockyer</td>
+            <td>1961</td>
+          </tr>
+          <tr>
+            <td>Witchy Woman</td>
+            <td>The Eagles</td>
+            <td>1972</td>
+          </tr>
+          <tr>
+            <td>Shining Star</td>
+            <td>Earth, Wind, and Fire</td>
+            <td>1975</td>
+          </tr>
+        </tbody>
+      </table>
      @endif
     </body>
     </html>
