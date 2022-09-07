ee34ad3dc23fa0818353491d422e5f3136503459
Paul Fruitful <fruitful2007@outlook.com>
2022-09-07 13:44:30 +0100

    the aplication view error

--- app/Http/Controllers/applicationControl.php
+++ app/Http/Controllers/applicationControl.php
@@ -36,7 +36,7 @@ class applicationControl extends Controller
     //Show all applications a user has received 
     public function index(){
         return view('applications',[
-            'applications'=>auth()->user()->listings->applications
+            'applications'=>auth()->user()->listings->application
         ]);
     }
 }
