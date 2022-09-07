b4e4c73c96b41028bc1e1ec026bd64b95d34d2d1
Paul Fruitful <fruitful2007@outlook.com>
2022-09-07 15:25:22 +0100

    Navbar fix

--- resources/views/layouts/boiler.blade.php
+++ resources/views/layouts/boiler.blade.php
@@ -60,7 +60,7 @@ x-data="{open:false,menu:false, lokasi:false}">
           </div>
         </div>
         <div class="flex lg:hidden">
-          <button onclick="menu=!menu" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out" aria-label="Main menu" aria-expanded="false">
+          <button @click="menu=!menu" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out" aria-label="Main menu" aria-expanded="false">
             <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
             </svg>
