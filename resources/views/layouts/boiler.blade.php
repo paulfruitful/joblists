6023267750e3b1a8fe5c2facb0e93b4a32d0557d
Paul Fruitful <fruitful2007@outlook.com>
2022-09-07 15:30:19 +0100

    Navbar fix

--- resources/views/layouts/boiler.blade.php
+++ resources/views/layouts/boiler.blade.php
@@ -141,7 +141,7 @@ x-data="{open:false,menu:false, lokasi:false}">
                       <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="">
                     </div>
                     <div class="-mr-2">
-                      <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                      <button type="button" @click="menu=!menu" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                         <span class="sr-only">Close main menu</span>
                         <!-- Heroicon name: outline/x -->
                         <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
@@ -150,7 +150,7 @@ x-data="{open:false,menu:false, lokasi:false}">
                       </button>
                     </div>
                   </div>
-                  <div class="px-2 pt-2 pb-3 space-y-1 " @click="menu=!menu" >
+                  <div class="px-2 pt-2 pb-3 space-y-1 "  >
                     <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Features</a>
       
                     <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Company</a>
