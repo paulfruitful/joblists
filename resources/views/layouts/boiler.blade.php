f252cb72a453b92b7c47a9233b59bd1c5100bd84
Paul Fruitful <fruitful2007@outlook.com>
2022-09-07 15:29:50 +0100

    Navbar fix

--- resources/views/layouts/boiler.blade.php
+++ resources/views/layouts/boiler.blade.php
@@ -150,7 +150,7 @@ x-data="{open:false,menu:false, lokasi:false}">
                       </button>
                     </div>
                   </div>
-                  <div class="px-2 pt-2 pb-3 space-y-1">
+                  <div class="px-2 pt-2 pb-3 space-y-1 " @click="menu=!menu" >
                     <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Features</a>
       
                     <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Company</a>
