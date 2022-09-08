4359a2e5370ce2afcd26adae5e65029c5047c79d
Paul Fruitful <fruitful2007@outlook.com>
2022-09-08 22:28:27 +0100

    The navbar fix

--- resources/views/layouts/boiler.blade.php
+++ resources/views/layouts/boiler.blade.php
@@ -93,7 +93,74 @@ x-data="{open:false,menu:false, lokasi:false}">
       
             <div>
               <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
-                
+                <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
+                  <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                    <div class="flex items-center justify-between w-full md:w-auto">
+                      <a href="#">
+                        <span class="sr-only">Workflow</span>
+                        <img alt="Workflow" class="h-8 w-auto sm:h-10" src="https://www.kindpng.com/picc/m/133-1337806_job-vector-seeker-job-logo-magnifying-glass-png.png">
+                      </a>
+                      <div class="-mr-2 flex items-center md:hidden">
+                        <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
+                          <span class="sr-only">Open main menu</span>
+                          <!-- Heroicon name: outline/menu -->
+                          <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+                    <a href="#" class="font-medium text-gray-500 hover:text-gray-900">About</a>
+      
+      
+                    <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Featured Jobs</a>
+      
+                    <a href="/register" class="font-medium text-red-500 hover:text-indigo-900">Register</a>
+      
+                    <a href="/login" class="font-medium text-red-600 hover:text-indigo500">Log in</a>
+                  </div>
+                </nav>
+              </div>
+      
+              <!--
+                Mobile menu, show/hide based on menu open state.
+      
+                Entering: "duration-150 ease-out"
+                  From: "opacity-0 scale-95"
+                  To: "opacity-100 scale-100"
+                Leaving: "duration-100 ease-in"
+                  From: "opacity-100 scale-100"
+                  To: "opacity-0 scale-95"
+              -->
+              <div class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+                <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                  <div class="px-5 pt-4 flex items-center justify-between">
+                    <div>
+                      <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="">
+                    </div>
+                    <div class="-mr-2">
+                      <button type="button" @click="menu=!menu" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <span class="sr-only">Close main menu</span>
+                        <!-- Heroicon name: outline/x -->
+                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                  
+    <div x-show="menu" class="block md:hidden">
+                  <div class="px-2 pt-2 pb-3 space-y-1 "  >
+                    <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Features</a>
+      
+                    <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Company</a>
+                  </div>
+                  <a href="/login" class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"> Log in </a>
+               
+              </div>
+            </div> </div>
       
             <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
               <div class="sm:text-center lg:text-left">
