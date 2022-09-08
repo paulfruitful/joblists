e0a3850f4df71321c13d2bb330b2f340b5edeb99
Paul Fruitful <fruitful2007@outlook.com>
2022-09-09 00:27:00 +0100

    Guest page rennovation

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -24,8 +24,8 @@
 <body>
     <section class="dark:bg-gray-800 dark:text-gray-100">
         <div class="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl">
-            <h1 class="text-4xl font-bold leading-none sm:text-5xl red-700">Register
-                <span class="text-indigo-700">Now</span>
+            <h1 class="text-4xl font-bold leading-none sm:text-5xl red-700">Join 
+                <span class="text-gray-700">Now</span>
             </h1>
             <p class="px-8 mt-8 mb-12 text-lg text-gray-700">Welcome to the registration page, click the get started buttton to register on our platform and get the right job for you today!</p>
             <div class="flex flex-wrap justify-center">
