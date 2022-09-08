04fd58c400fc43dfcf8f7abc9dcba9decbdfc76a
Paul Fruitful <fruitful2007@outlook.com>
2022-09-09 00:57:35 +0100

    The border arrangement of the job component

--- resources/views/components/listing-comp.blade.php
+++ resources/views/components/listing-comp.blade.php
@@ -9,7 +9,7 @@
 
 <div class="max-w-sm relative">
     <a href="/{{$list->id}}">
-<div class="border-2 relative border-gray box-content block border-rounded overflow-hidden p-12 mb-6 flex hover:border-indigo-500 w-screen shadow-lg shadow-blacks w-full ">
+<div class="border-2 relative border-gray box-content block border-rounded overflow-hidden p-12 mb-6 flex hover:border-gray-900 w-screen shadow-lg shadow-blacks w-full ">
    <a href="/{{$list->id}}"> <h2 class="text-2xl text-black  " >{{$list->title}}</h2></a>
   
 </div></a>
