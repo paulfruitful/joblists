63da16ff4f0ce69a90fc3c85373f7b4a48934a91
Paul Fruitful <fruitful2007@outlook.com>
2022-09-08 23:35:54 +0100

    The navbar fix

--- resources/views/layouts/boiler.blade.php
+++ resources/views/layouts/boiler.blade.php
@@ -101,8 +101,7 @@ x-data="{open:false,menu:false, lokasi:false}">
                         <img alt="Workflow" class="h-8 w-auto sm:h-10" src="https://www.kindpng.com/picc/m/133-1337806_job-vector-seeker-job-logo-magnifying-glass-png.png">
                       </a>
                       <div class="-mr-2 flex items-center md:hidden">
-                        <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
-                          <span class="sr-only">Open main menu</span>
+                           <span class="sr-only">Open main menu</span>
                           <!-- Heroicon name: outline/menu -->
                           <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
