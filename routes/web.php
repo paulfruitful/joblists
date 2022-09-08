919b238b635fd947186836e796cd09e9688fe0b2
Paul Fruitful <fruitful2007@outlook.com>
2022-09-09 00:51:08 +0100

    The error redirect page

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::get('/manage',function(){
    }
     return view('manage',[
         'listings'=>auth()->user()->listing
-    ])->middleware('auth');
+    ]);
 });
 Route::get('/applications',[applicationControl::class,'index'])->middleware('auth');
 //The route to get the listing creation form 
