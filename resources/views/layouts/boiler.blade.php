54d51356fe091460f9e94ee476db3307d0ea4a6a
Paul Fruitful <fruitful2007@outlook.com>
2022-09-09 00:17:02 +0100

    Guest page rennovation

--- resources/views/layouts/boiler.blade.php
+++ resources/views/layouts/boiler.blade.php
@@ -105,7 +105,7 @@ x-data="{open:false,menu:false, lokasi:false}">
               </div>
           </div>
       </div>
-      <div class="hidden lg:block lg:w-1/2" style="clip-path:polygon(10% 0, 100% 0%, 100% 100%, 0 100%)">
+      <div class="hidden lg:block lg:w-1/2 lg:h-full" style="clip-path:polygon(10% 0, 100% 0%, 100% 100%, 0 100%)">
           <div class="h-full object-cover" style="background-image: url(https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80)">
               <div class="h-full bg-black opacity-25"></div>
           </div>
