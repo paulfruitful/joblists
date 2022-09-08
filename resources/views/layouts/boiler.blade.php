744f974ceabf2dcf330cac7d4711e1bddc430a42
Paul Fruitful <fruitful2007@outlook.com>
2022-09-08 23:59:29 +0100

    Middlewares

--- resources/views/layouts/boiler.blade.php
+++ resources/views/layouts/boiler.blade.php
@@ -83,48 +83,47 @@ x-data="{open:false,menu:false, lokasi:false}">
 @include('components.hero')
 <x-popup-message/>
    @yield('content')
- @else
-    <div class="relative bg-white overflow-hidden">
-       
-      
-              <!--
-                Mobile menu, show/hide based on menu open state.
-      
-                Entering: "duration-150 ease-out"
-                  From: "opacity-0 scale-95"
-                  To: "opacity-100 scale-100"
-                Leaving: "duration-100 ease-in"
-                  From: "opacity-100 scale-100"
-                  To: "opacity-0 scale-95"
-              -->
-              <div class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-             
-           
-            </div> </div>
-      
-            <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-              <div class="sm:text-center lg:text-left">
-                <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                  <span class="block xl:inline">Get The Right</span>
-                  <span class="block text-red-600 xl:inline"> Job Today</span>
-                </h1>
-                <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">Are You Looking For A Job? <br> Joblists got your back. We are a job sharing platform that aims at giving junior developers short-term contracts and internship roles.</p>
-                <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                  <div class="rounded-md shadow">
-                    <a href="/register" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"> Sign Up </a>
-                  </div>
-                  <div class="mt-3 sm:mt-0 sm:ml-3">
-                    <a href="/login" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-black-700 bg-red-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"> Sign In </a>
-                  </div>
-                </div>
+ @else<div class="w-full">
+  <nav class="bg-white shadow-lg">
+      <div class="md:flex items-center justify-between py-2 px-8 md:px-12">
+          <div class="flex justify-between items-center">
+             <div class="text-2xl font-bold text-gray-800 md:text-3xl">
+                  <a href="#">Brand</a>
+             </div>
+              <div class="md:hidden">
+                  <button type="button" class="block text-gray-800 hover:text-gray-700 focus:text-gray-700 focus:outline-none">
+                      <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                          <path class="hidden" d="M16.24 14.83a1 1 0 0 1-1.41 1.41L12 13.41l-2.83 2.83a1 1 0 0 1-1.41-1.41L10.59 12 7.76 9.17a1 1 0 0 1 1.41-1.41L12 10.59l2.83-2.83a1 1 0 0 1 1.41 1.41L13.41 12l2.83 2.83z"/>
+                          <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
+                      </svg>
+                  </button>
               </div>
-            </main>
           </div>
-        </div>
-        <div class="lg:absolute lg:inset-y-1 lg:right-0 lg:w-1/2">
-          <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt="">
-        </div>   
+          <div class="flex flex-col md:flex-row hidden md:block -mx-2">
+              <a href="#" class="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">Home</a>
+              <a href="#" class="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">About</a>
+              <a href="#" class="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">Contact</a>
+          </div>
       </div>
+  </nav>
+  <div class="flex bg-white" style="height:600px;">
+      <div class="flex items-center text-center lg:text-left px-8 md:px-12 lg:w-1/2">
+          <div>
+              <h2 class="text-3xl font-semibold text-gray-800 md:text-4xl">Build Your New <span class="text-indigo-600">Idea</span></h2>
+              <p class="mt-2 text-sm text-gray-500 md:text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi cum cupiditate ducimus, fugit harum id necessitatibus odio quam quasi, quibusdam rem tempora voluptates. Cumque debitis dignissimos id quam vel!</p>
+              <div class="flex justify-center lg:justify-start mt-6">
+                  <a class="px-4 py-3 bg-gray-900 text-gray-200 text-xs font-semibold rounded hover:bg-gray-800" href="#">Get Started</a>
+                  <a class="mx-4 px-4 py-3 bg-gray-300 text-gray-900 text-xs font-semibold rounded hover:bg-gray-400" href="#">Learn More</a>
+              </div>
+          </div>
+      </div>
+      <div class="hidden lg:block lg:w-1/2" style="clip-path:polygon(10% 0, 100% 0%, 100% 100%, 0 100%)">
+          <div class="h-full object-cover" style="background-image: url(https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80)">
+              <div class="h-full bg-black opacity-25"></div>
+          </div>
+      </div>
+  </div>
+</div>
   @endauth
     </body>
 </html>
