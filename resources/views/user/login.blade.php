550800b5c86b83bbd8102ce0e01e884e599d352b
Paul Fruitful <fruitful2007@outlook.com>
2022-09-09 00:21:31 +0100

    Guest page rennovation

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -51,7 +51,7 @@
     <div class="md:flex md:items-center">
         <div class="md:w-1/3"></div>
         <div class="md:w-2/3">
-          <button class="shadow bg-red-600 hover:bg-indigo-600 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+          <button class="shadow bg-gray-900 hover:bg-indigo-600 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
             Sign in
           </button>
         </div>
