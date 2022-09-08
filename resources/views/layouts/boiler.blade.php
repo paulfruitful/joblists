592467f6942327b11fee029c4486deb6bfdbb13e
Paul Fruitful <fruitful2007@outlook.com>
2022-09-09 00:11:23 +0100

    Guest page rennovation

--- resources/views/layouts/boiler.blade.php
+++ resources/views/layouts/boiler.blade.php
@@ -90,14 +90,7 @@ x-data="{open:false,menu:false, lokasi:false}">
              <div class="text-2xl font-bold text-gray-800 md:text-3xl">
                   <a href="#">Joblists</a>
              </div>
-              <div class="md:hidden">
-                  <button type="button" class="block text-gray-800 hover:text-gray-700 focus:text-gray-700 focus:outline-none">
-                      <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-                          <path class="hidden" d="M16.24 14.83a1 1 0 0 1-1.41 1.41L12 13.41l-2.83 2.83a1 1 0 0 1-1.41-1.41L10.59 12 7.76 9.17a1 1 0 0 1 1.41-1.41L12 10.59l2.83-2.83a1 1 0 0 1 1.41 1.41L13.41 12l2.83 2.83z"/>
-                          <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
-                      </svg>
-                  </button>
-              </div>
+           
           </div>
         
   </nav>
