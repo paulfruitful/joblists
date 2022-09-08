d7debd6ca016bf1e237cad84ee5741210b1a187e
Paul Fruitful <fruitful2007@outlook.com>
2022-09-09 00:08:49 +0100

    Guest page rennovation

--- resources/views/layouts/boiler.blade.php
+++ resources/views/layouts/boiler.blade.php
@@ -105,7 +105,7 @@ x-data="{open:false,menu:false, lokasi:false}">
       <div class="flex items-center text-center lg:text-left px-8 md:px-12 lg:w-1/2">
           <div>
               <h2 class="text-3xl font-semibold text-gray-800 md:text-4xl">Get A Job<span class="text-indigo-600">Today</span></h2>
-              <p class="mt-2 text-sm text-gray-500 md:text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi cum cupiditate ducimus, fugit harum id necessitatibus odio quam quasi, quibusdam rem tempora voluptates. Cumque debitis dignissimos id quam vel!</p>
+              <p class="mt-2 text-sm text-gray-500 md:text-base">Getting A Job Or An Internship Role As A Junior Developer Or Fresher Can Be Tiring.<br> Joblists was built for you to make finding jobs and gigs easily, join the network today by clicking the sign up button. </p>
               <div class="flex justify-center lg:justify-start mt-6">
                   <a class="px-4 py-3 bg-gray-900 text-gray-200 text-xs font-semibold rounded hover:bg-gray-800" href="#">Get Started</a>
                   <a class="mx-4 px-4 py-3 bg-gray-300 text-gray-900 text-xs font-semibold rounded hover:bg-gray-400" href="#">Learn More</a>
