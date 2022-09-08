436f1aaecc99407235e4a31e933961b3b691b8b0
Paul Fruitful <fruitful2007@outlook.com>
2022-09-09 00:39:55 +0100

    Guest page rennovation

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -50,7 +50,7 @@
                 <label class="block uppercase tracking-wide text-indigo-700 text-xs font-bold mb-2" for="grid-password">
                   Email
                 </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="email" placeholder="youreamil@example.com" name="email">
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="email" placeholder="emaill@example.com" name="email">
                @error('email')
             <div class="w-full"><p class="text-red-500">{{$message}}</p></div>
                @enderror
