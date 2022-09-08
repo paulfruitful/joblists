70acb8de3894bafcd52ed687c68f19551b44985f
Paul Fruitful <fruitful2007@outlook.com>
2022-09-09 00:40:24 +0100

    Guest page rennovation

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -47,7 +47,7 @@
                    @enderror
             </div>
             <div class="w-full px-3">
-                <label class="block uppercase tracking-wide text-indigo-700 text-xs font-bold mb-2" for="grid-password">
+                <label class="block uppercase tracking-wide text-gray-900 text-xs font-bold mb-2" for="grid-password">
                   Email
                 </label>
                 <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="email" placeholder="emaill@example.com" name="email">
