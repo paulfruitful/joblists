7f523bb37d4e1f71d011f13aed38aa0cf0c99e6a
Paul Fruitful <fruitful2007@outlook.com>
2022-09-10 01:55:18 +0100

    Adding the user notification

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -26,7 +26,7 @@ class userController extends Controller
         );
         $formData['password']=bcrypt($formData['password']);
         $user=User::create($formData);
-        Notification::send($user,new Welcome());
+        Notification::send($user,new Welcome($user));
   
         auth()->login($user);
      return redirect('/')->with('success','Account Created Successfully');
