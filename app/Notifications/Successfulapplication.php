7d0d91e7f84b137f683cedf12c19d33d2b8bb291
Paul Fruitful <fruitful2007@outlook.com>
2022-09-10 13:38:07 +0100

    Creating a notification Queue

--- app/Notifications/Successfulapplication.php
+++ app/Notifications/Successfulapplication.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class Successfulapplication extends Notification
+class Successfulapplication extends Notification implements ShouldQueue
 {
     use Queueable;
     private $name;
