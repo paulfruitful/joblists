71d7b72266b789f7981db7ae725fdd272f628872
Paul Fruitful <fruitful2007@outlook.com>
2022-09-12 00:17:00 +0100

    the application-data view

--- resources/views/application-data.blade.php
+++ resources/views/application-data.blade.php
@@ -49,7 +49,7 @@
                 </aside>
                 <main role="main" class="w-full sm:w-2/3 md:w-3/4 pt-1 px-2">
                     <h1 class="text-2xl" id="home">Applicant's Information</h1>
-                    <p class="text-gray-700 font-semibold">  </p>
+                    <p class="text-gray-700 font-semibold"> Check Out The Applicant's Name, Email, Curriculum Vitae and Portfolio </p>
      
                     <ul>
                         <li><span class="text-purple-700">flex</span> - for <code>dislay:flex</code></li>
