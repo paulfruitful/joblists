8065fd67d9f8602036b267c59c184becc3ae9dad
Paul Fruitful <fruitful2007@outlook.com>
2022-09-12 00:32:17 +0100

    the application-data view

--- resources/views/application-data.blade.php
+++ resources/views/application-data.blade.php
@@ -53,7 +53,7 @@
      
                     <ul class="mt-4">
                         <li class="  border-solid border-b-2 border-gray-400 "><span class="text-gray-900 font-bold ">Name: </span> {{$application->name}}</li>
-                        <li class="  border-solid border-b-2 border-gray-400 "><span class="text-gray-900 font-bold ">Name: </span> {{$application->name}}</li>
+                        <li class="  border-solid border-b-2 border-gray-400 "><span class="text-gray-900 font-bold ">Email: </span> {{$application->email}}</li>
                         <li class="  border-solid border-b-2 border-gray-400 "><span class="text-gray-900 font-bold ">Name: </span> {{$application->name}}</li>
                         <li class="  border-solid border-b-2 border-gray-400 "><span class="text-gray-900 font-bold ">Name: </span> {{$application->name}}</li>
                       
