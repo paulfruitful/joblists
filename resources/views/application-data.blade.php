88614386ccae46f9d0bdfd2f362b0450fd78c0ff
Paul Fruitful <fruitful2007@outlook.com>
2022-09-12 01:27:49 +0100

    The footer component

--- resources/views/application-data.blade.php
+++ resources/views/application-data.blade.php
@@ -68,38 +68,6 @@
                 </main>
             </div>
         </div>
-        <footer class="bg-black mt-auto">
-            <div class="container p-4 text-white mx-auto">
-                <h1 class="text-2xl" >Footer</h1>
-                <div class="flex">
-                    <div class="flex-grow flex flex-col">
-                        <a href="/#home">Boom</a>
-                        <a href="#">Boom</a>
-                        <a href="#">Boom</a>
-                        <a href="#">Boom</a>
-                    </div>
-                    <div class="flex-grow flex flex-col">
-                        <a href="#">Boom</a>
-                        <a href="#">Boom</a>
-                        <a href="#">Boom</a>
-                        <a href="#">Boom</a>
-                    </div>
-                    <div class="flex-grow flex flex-col">
-                        <a href="#">Boom</a>
-                        <a href="#">Boom</a>
-                        <a href="#">Boom</a>
-                    </div>
-                    <div class="flex-grow flex flex-col">
-                        <a href="#">Boom</a>
-                        <a href="#">Boom</a>
-                        <a href="#">Boom</a>
-                        <a href="#">Boom</a>
-                    </div>
-                </div>
-                <div class="text-right text-xs py-4">
-                    <a href="">&copy;2021 Someco Inc.</a>
-                </div>
-            </div>
-        </footer>
+
     </body>
 </html>
