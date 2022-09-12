a6508a751199e8737ffdf235eac2d6081b587a52
Paul Fruitful <fruitful2007@outlook.com>
2022-09-12 01:25:07 +0100

    The no-image jpg

--- resources/views/components/job-list.blade.php
+++ resources/views/components/job-list.blade.php
@@ -7,7 +7,7 @@
       <div class="block rounded-lg shadow-lg bg-white">
         <div class="flex flex-wrap items-center">
           <div class="hidden lg:flex grow-0 shrink-0 basis-auto lg:w-6/12 xl:w-4/12">
-            <img src="{{$list->photo ? asset('storage/' . $list->photo) : asset('/images/no-image.jpg')}}" alt="Trendy Pants and Shoes"
+            <img src="{{$list->photo ? asset('storage/' . $list->photo) : asset('storage/photo/no-image.jpg')}}" alt="Trendy Pants and Shoes"
               class="w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg" />
           </div>
           <div class="grow-0 shrink-0 basis-auto w-full lg:w-6/12 xl:w-8/12">
