ad767dc487844dbfeb6f776f99774ac45d747aa0
Paul Fruitful <fruitful2007@outlook.com>
2022-09-14 19:08:54 +0100

    Re-desiging the UI of the app

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -43,7 +43,7 @@
              @csrf
              @method('DELETE')
              <button type="submit" class="  right-0  justify-center relative py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700">Delete</button>
-              <a href="/{{$list->id}}/edit" class="py-2  px-4 bg-indigo-700 text-white border border-transparent rounded-md">Edit List</a>
+              <a href="/{{$list->id}}/edit" class="py-2  px-4 bg-gray-900 text-white border border-transparent rounded-md">Edit List</a>
            </form> </div>
         </div> 
         </div>
