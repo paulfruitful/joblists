974a0cf7f86442c20ce342a8ddf054075b3223c0
Paul Fruitful <fruitful2007@outlook.com>
2022-09-14 22:54:36 +0100

    Api resource transformer

--- app/Http/Resources/listCollection.php
+++ app/Http/Resources/listCollection.php
@@ -3,7 +3,7 @@ namespace App\Http\Resources;
  
 use Illuminate\Http\Resources\Json\JsonResource;
  
-class listingCollection extends JsonResource
+class listCollection extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
