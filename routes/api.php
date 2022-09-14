01c8abec6713cddd8c70339cbb57aa81b33b79dc
Paul Fruitful <fruitful2007@outlook.com>
2022-09-14 22:27:17 +0100

    The api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::get('/listing',function(Listing $listing){
+
 });
