dbc87e6bb2867bdce67b2235d6d37acea856a36e
Paul Fruitful <fruitful2007@outlook.com>
2022-09-25 12:21:40 +0100

    Some new changes and modifications

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,13 +3,13 @@
         <h1 class="text-2xl mb-4 font-bold" >Joblists</h1>
         <div class="flex">
             <div class="flex-grow flex flex-col">
-                <a href="/">Home</a>
+                
                 <a href="/listings">Jobs</a>
                 <a href="/manage">Applications</a>
                
             </div>
             <div class="flex-grow flex flex-col">
-                <a href="/api">APIs</a>
+             
                 <a href="/manage">Listings</a>
                 <a href="mailto:fruitful2007@outlook.com">Get Source Code</a>
             </div>
